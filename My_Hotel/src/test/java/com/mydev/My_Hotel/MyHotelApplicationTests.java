5d709ece0e3f311384ebf88e914d941a7b195a39
azdine06 <oazdine06@gmail.com>
2025-04-15 15:14:11 +0100

    first commit

---
+++ My_Hotel/src/test/java/com/mydev/My_Hotel/MyHotelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mydev.My_Hotel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyHotelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
